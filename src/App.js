d41243e42fc4a6a15fa79ee2a6dd5e401e95f4b2
Robert Nascimento <robert1a7x@gmail.com>
2022-05-10 12:48:59 -0300

    configuracao inicial do projeto, instaladas algumas dependencias e criada a estrutura inicial de paginas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from '../src/pages/Login';
+import Products from '../src/pages/Products';
+import Dashboard from '../src/pages/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path='/' element={ <Login /> } />
+      <Route exact path='/products' element={ <Products /> } />
+      <Route exact path='/dashboard' element={ <Dashboard /> } />
+    </Routes>
   );
 }
 
