7fef94dc064337b0540537e4ecb7e79455e3a2b9
Robert Nascimento <robert1a7x@gmail.com>
2022-05-12 20:11:45 -0300

    implementada a solucao para mostrar os produtos do banco na tela de produtos

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import DashboardButton from '../components/DashboardButton';
 import Header from '../components/Header'
+import { requestData, setToken } from '../services/requests';
 
 const Products = () => {
 	const [isAdm, setIsAdm] = useState(false);
 	const [user, setUser] = useState();
+	const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem('user'));
 		if (user.role === 'admin') setIsAdm(true);
 		setUser(user);
+
+		const fetchProducts = async () => {
+			const endpoint = '/products';
+			setToken(user.token);
+			const data = await requestData(endpoint);
+	
+		setProducts(data);
+		}
+
+		fetchProducts();
   }, []);
 
 	return (
@@ -20,7 +32,16 @@ const Products = () => {
 				? <DashboardButton />
 				: null
 			}
-			<div>Products</div>
+			<div className="products-container">
+				{ products.map(({ name, description, price, image }) => (
+					<div key={ name }>
+						<p>{ name }</p>
+						<p>{ description }</p>
+						<p>{ price }</p>
+						<img src={ image } alt={ name } />
+					</div>
+				)) }
+			</div>
 		</div>
 	)
 }
